a001e80236d8deae1d753244086cd3ecf5df5271
Josiah <63004866+Necl0@users.noreply.github.com>
2022-08-24 11:48:14 -0400

    Update NN.py

--- NN.py
+++ NN.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import List
-from numpy import exp
+import numpy as np
 from random import uniform
 
 layer_n = int(input("How many layers are in the network? "))
@@ -12,7 +12,7 @@ for layer in range(1, layer_n+1):
   layer_weights.append([uniform(0, 1) for i in range((nodes))])
 
 
-bias = float(input("Enter in the bias: "))
+bias = [uniform(0.0, 0.1) for j in range(layer_n)]
 threshold = float(input("Enter in the threshold: "))
 
 layer_one = [*map(float, input(f"\nEnter in the inputs for the first layer ({len(layer_inputs[0])} inputs): ").split())]
@@ -24,7 +24,7 @@ class NeuralNetwork:
   layer_n: int
   layer_inputs: List[List[int]]
   layer_weights: List[List[float]]
-  bias: float
+  bias: List[float]
   threshold: float
 
   def relu(self, num: int) -> int:
@@ -33,24 +33,21 @@ class NeuralNetwork:
 
   def sigmoid(self, num: int) -> float:
     """Sigmoid activation function: last layer"""
-    return 1/(1+exp(-num))
+    return 1/(1+np.exp(-num))
 
 @dataclass
 class Perceptron:
   """Perceptron Class"""
   inputs: List[int]
   weights: List[int]
-  bias: int
+  bias: List[float]
   threshold: int
 
 
 def percep_output(inputs, weights, bias, treshold) -> int:
-    return int((sum([input*weight for (input, weight) in zip(inputs, weights)]) + bias) > threshold)
+    return int(np.dot(inputs, weights)+ bias > threshold)
 
 n1 = NeuralNetwork(layer_n, layer_inputs, layer_weights, bias, threshold)
 
-def feedforward(num_layers, layer_vals, layer_weights, bias, treshold) -> float:
+def feedforward(num_layers, layer_inputs, layer_weights, bias, treshold) -> int:
   ouptut_n = int # output neuron
-
-
-  return output_n
