46f87c9d2475cfaa5ce44f564202ab1b0b5e41fb
Josiah <63004866+Necl0@users.noreply.github.com>
2022-09-05 10:53:52 -0400

    Update NN.py

--- NN.py
+++ NN.py
@@ -12,18 +12,20 @@ for layer in range(1, layer_n+1):
   layer_inputs.append([float() for i in range(nodes)])
 
 
+
 bias = [uniform(0.0, 0.1) for j in range(layer_n)] # generate random bias values for each layer in the network
 threshold = 0.5 # default treshold value, halfway from 0-1
 layer_one = [*map(float, input(f"\nEnter in the inputs for the first layer ({len(layer_inputs[0])} inputs): ").split())]
 layer_inputs[0] = layer_one # set inputs to first layer of network
 
+# weight generation
 for i in range(2, layer_n+1):
   for node in layer_inputs[i-1]:
     layer_weights.append([uniform(0,1) for i in range(len(layer_inputs[i-2]))])
     
 @dataclass
 class NeuralNetwork:
-  """Neural Network class"""
+  """Neural Network dataclass"""
   layer_n: int
   layer_inputs: List[List[int]]
   layer_weights: List[List[float]]
@@ -31,11 +33,11 @@ class NeuralNetwork:
   threshold: float
 
 def relu( num: int) -> int:
-  """Relu activation function: all layers except last"""
+  """Relu activation: all layers except last"""
   return max(0, num)
 
 def sigmoid(num: int) -> float:
-  """Sigmoid activation function: last layer"""
+  """Sigmoid activation: last layer"""
   return 1/(1+np.exp(-num))
 
 @dataclass
@@ -48,19 +50,20 @@ class Perceptron:
 
 
 def percepOutput(inputs: List[List[int]], weights: List[List[float]], bias: List[float]) -> float:
+    """Perceptron output"""
     return float(np.dot(inputs, weights) + bias)
 
 def forwardFeed(network) -> List[List[int]]:
-  """Forward Feed function"""
-  # iterate through each layer
-  for i in range(1, network.layer_n): 
-    # iterate through each perceptron  
-    c = 0
-    for percep in network.layer_inputs[i]:
-      network.layer_inputs[i][c] = percepOutput(network.layer_inputs[i-1], network.layer_weights[i-1], network.bias[i-1])
-      print(network.layer_weights[i-1])
+  """Forward Feed"""
+  c = 0
+  for n in range(1, network.layer_n):
+    for node in network.layer_inputs[n]:
+      node = percepOutput(network.layer_inputs[n-1], layer_weights[c] , network.bias[n-1])
+      print(layer_weights[c])
       c+=1
-  return network.layer_inputs
+  return network.layer_inputs[-1]
 
 n1 = NeuralNetwork(layer_n, layer_inputs, layer_weights, bias, threshold)
 output = forwardFeed(n1)
+
+print(f"The output from the Neural Network is {output}")
